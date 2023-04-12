943d2387560201127e0cbee24d185345227557ad
santiagof <santiagofritz97@gmail.com>
2023-04-11 22:36:29 -0300

    Se agregaron los otros 2 layouts y se agrego la logica de inicio de sesion.

--- app/src/main/java/com/example/myloggin/MainActivity.kt
+++ app/src/main/java/com/example/myloggin/MainActivity.kt
@@ -1,11 +1,38 @@
 package com.example.myloggin
 
+import android.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.EditText
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+
+    fun loginFunction(view: View) {
+        val data = mapOf("Juan Torres" to "1234utn")
+        val userName = findViewById<EditText>(R.id.userName).text.toString()
+        val userPasswd = findViewById<EditText>(R.id.userPasswd).text.toString()
+        if (userName.isEmpty() || userPasswd.isEmpty()){
+            val message = AlertDialog.Builder(this)
+            message.setTitle("Error al iniciar sesión")
+            message.setMessage("Complete los datos de inicio")
+            val popup = message.create()
+            popup.show()
+        }
+        if (data[userName]==userPasswd){
+            val message = AlertDialog.Builder(this)
+            message.setTitle("Error al iniciar sesión")
+            message.setMessage("Usuario inexistente o contraseña incorrecta")
+            val popup = message.create()
+            popup.show()
+        }
+    }
+
 }
