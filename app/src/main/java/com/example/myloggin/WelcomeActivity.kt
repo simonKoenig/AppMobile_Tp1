b9ba909039d0de054a13da48037f3412e39838a2
Santiago Di Camillo <dicamillosantiago@gmail.com>
2023-04-13 14:32:49 -0300

    Agrego select de sistema operativo y caja de texto para agregar preferencias

--- app/src/main/java/com/example/myloggin/WelcomeActivity.kt
+++ app/src/main/java/com/example/myloggin/WelcomeActivity.kt
@@ -3,6 +3,10 @@ package com.example.myloggin
 import android.os.Bundle
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import android.app.AlertDialog
+import android.view.View
+import android.widget.EditText
+
 
 class WelcomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -12,6 +16,24 @@ class WelcomeActivity : AppCompatActivity() {
         val userName = intent.getStringExtra("userName")
         val message = "Bienvenido $userName"
         welcomeText.text = message
+    }
+
+    fun showDialogFunction(view: View) {
+        val builder = AlertDialog.Builder(this)
+        builder.setTitle("Ingrese su texto")
+
+        val input = EditText(this)
+        builder.setView(input)
+
+        builder.setPositiveButton("Aceptar") { _, _ ->
+            val text = input.text.toString()
+        }
+        builder.setNegativeButton("Cancelar") { dialog, _ ->
+            dialog.cancel()
+        }
+
+        val dialog = builder.create()
+        dialog.show()
     }
 
 }
