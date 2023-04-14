b6e6f58e80466184bdd8c188f78e0df8cdf823f5
Santiago Di Camillo <dicamillosantiago@gmail.com>
2023-04-14 17:01:57 -0300

    Agrego funcionalidad de seleccionar S.O. (con bug)

--- app/src/main/java/com/example/myloggin/WelcomeActivity.kt
+++ app/src/main/java/com/example/myloggin/WelcomeActivity.kt
@@ -6,6 +6,9 @@ import androidx.appcompat.app.AppCompatActivity
 import android.view.View
 import android.widget.CheckBox
 import android.widget.EditText
+import android.widget.ImageView
+import android.widget.RadioButton
+import android.widget.RadioGroup
 
 
 class WelcomeActivity : AppCompatActivity() {
@@ -18,6 +21,22 @@ class WelcomeActivity : AppCompatActivity() {
         welcomeText.text = message
         val editText = findViewById<EditText>(R.id.otro)
         editText.visibility = View.INVISIBLE
+
+        //iría en otra función
+        val androidCheck = findViewById<RadioButton>(R.id.android)
+        val iosCheck = findViewById<RadioButton>(R.id.ios)
+        val androidFoto = findViewById<ImageView>(R.id.androidFoto)
+        val iosFoto = findViewById<ImageView>(R.id.iosFoto)
+
+        androidCheck.setOnCheckedChangeListener { buttonView, isChecked ->
+            androidFoto.visibility = View.VISIBLE
+            iosFoto.visibility = View.GONE
+        }
+
+        iosCheck.setOnCheckedChangeListener { buttonView, isChecked ->
+            iosFoto.visibility = View.VISIBLE
+            androidFoto.visibility = View.GONE
+        }
     }
 
     fun desplegarTexto(view: View) {
@@ -28,5 +47,23 @@ class WelcomeActivity : AppCompatActivity() {
         } else {
             editText.visibility = View.INVISIBLE
         }
-        }
     }
+
+    /*fun cambiarFoto(view: View) {
+        val androidCheck = findViewById<RadioButton>(R.id.android)
+        val iosCheck = findViewById<RadioButton>(R.id.ios)
+        val androidFoto = findViewById<ImageView>(R.id.androidFoto)
+        val iosFoto = findViewById<ImageView>(R.id.iosFoto)
+
+        androidCheck.setOnCheckedChangeListener { buttonView, isChecked ->
+            androidFoto.visibility = View.VISIBLE
+            iosFoto.visibility = View.GONE
+        }
+
+        iosCheck.setOnCheckedChangeListener { buttonView, isChecked ->
+            iosFoto.visibility = View.VISIBLE
+            androidFoto.visibility = View.GONE
+        }
+    }*/
+}
+
